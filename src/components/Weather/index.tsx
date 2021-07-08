53b3a63f06469d4141bc3daa0580f82e787a09dd
berkeatac <berke.atac@gmail.com>
2021-07-09 01:10:40 +0200

    Feat: Design weather info. card

--- src/components/Weather/index.tsx
+++ src/components/Weather/index.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState, useRef } from "react";
 import { useGetCityByNameLazyQuery } from "../../generated/graphql";
 import _ from "lodash";
-import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import { MapContainer, TileLayer } from "react-leaflet";
 
 const mapProps = {
   doubleClickZoom: false,
@@ -11,6 +11,8 @@ const mapProps = {
   touchZoom: false,
   scrollWheelZoom: false,
   zoomControl: false,
+  zoom: 13,
+  attributionControl: false,
 };
 
 const Weather = () => {
@@ -48,58 +50,76 @@ const Weather = () => {
         onChange={(e) => setCity(e.target.value)}
         value={city}
       ></input>
-      {!loading && (
+      {!loading && !error && data?.getCityByName && (
         <>
-          {summary && (
-            <>
-              <h2>{summary.title}</h2>
-              <p>{summary.description}</p>
-            </>
-          )}
-          {temperature && (
-            <>
-              <h4>{temperature.actual}</h4>
-              <ul>
-                <li>
-                  <b>Feels like</b>: {temperature.feelsLike}
-                </li>
-                <li>
-                  <b>Min</b>: {temperature.min}
-                </li>
-                <li>
-                  <b>Max</b>: {temperature.max}
-                </li>
-              </ul>
-            </>
-          )}
-          {data?.getCityByName?.coord && (
-            <div className="flex justify-center">
-              <MapContainer
-                style={{ height: "30vh", width: "60vh" }}
-                center={[
-                  data?.getCityByName?.coord?.lat || 51.505,
-                  data?.getCityByName?.coord?.lon || -0.09,
-                ]}
-                zoom={13}
-                {...mapProps}
-              >
-                <TileLayer
-                  attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-                  url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-                />
-                <Marker
-                  position={[
-                    data?.getCityByName?.coord?.lat || 51.505,
-                    data?.getCityByName?.coord?.lon || -0.09,
-                  ]}
-                >
-                  <Popup>
-                    A pretty CSS3 popup. <br /> Easily customizable.
-                  </Popup>
-                </Marker>
-              </MapContainer>
+          <div className="flex justify-center mt-6">
+            <div className="bg-white shadow-lg rounded-3xl p-4 w-full h-full lg:w-3/4 lg:h-1/4 overflow-hidden">
+              <div className="flex-none lg:flex">
+                <div className=" h-full w-full lg:h-48 lg:w-48 lg:mb-0 mb-3">
+                  {data?.getCityByName?.coord && (
+                    <div className="flex justify-center">
+                      <MapContainer
+                        className=" h-full w-full lg:h-48 lg:w-48 lg:mb-0 mb-3 rounded-xl"
+                        center={[
+                          data?.getCityByName?.coord?.lat || 51.505,
+                          data?.getCityByName?.coord?.lon || -0.09,
+                        ]}
+                        {...mapProps}
+                      >
+                        <TileLayer
+                          attribution='&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
+                          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                        />
+                      </MapContainer>
+                    </div>
+                  )}
+                </div>
+                <div className="flex flex-col ml-4 py-2 justify-around">
+                  <div className="">
+                    <h2 className="flex text-lg font-medium">
+                      {summary?.title}
+                    </h2>
+                    <h2 className="flex text-sm">{summary?.description}</h2>
+                    <p className="mt-3"></p>
+                    <h2 className="flex text-lg font-medium">
+                      {temperature?.actual} C
+                    </h2>
+                    <h2 className="flex text-sm">
+                      {temperature?.min} C - {temperature?.max} C
+                    </h2>
+                  </div>
+                  <p className="mt-3"></p>
+                  <div className="flex py-4  text-sm text-gray-600">
+                    <div className="flex-1 inline-flex items-center">
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        className="h-5 w-5 mr-3"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M17.657 16.657L13.414 20.9a1.998 1.998 0 01-2.827 0l-4.244-4.243a8 8 0 1111.314 0z"
+                        ></path>
+                        <path
+                          stroke-linecap="round"
+                          stroke-linejoin="round"
+                          stroke-width="2"
+                          d="M15 11a3 3 0 11-6 0 3 3 0 016 0z"
+                        ></path>
+                      </svg>
+                      <p className="">
+                        {`${data?.getCityByName?.name}, ${data?.getCityByName?.country}`}
+                      </p>
+                    </div>
+                  </div>
+                </div>
+              </div>
             </div>
-          )}
+          </div>
         </>
       )}
     </div>
